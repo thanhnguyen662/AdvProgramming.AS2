f723627bd5ddbb0467ed01bceb18c4724b8c1c65
thanhnguyen662 <56751762+thanhnguyen662@users.noreply.github.com>
2020-05-02 20:25:45 +0700

    Update 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hello World!: {0}", a);
+        }
+    }
+}
--- Meow_Meow_Cattery/Program.cs
+++ Meow_Meow_Cattery/Program.cs
@@ -13,13 +13,6 @@ namespace Meow_Meow_Cattery
             Console.OutputEncoding = Encoding.UTF8;
             Console.WriteLine("================== (^•,•^) ==================");
 
-            //Test Cat
-            Cat SphynxMeow = new Cat(1, eBreeds.Sphynx, "Yellow");
-            SphynxMeow.infoCat();
-
-            Console.WriteLine();
-            Console.ReadKey();
-
             //Tạo kho
             Repository MeowMeowCatteryRepo = new Repository();
             //Thêm mèo vào kho tương ứng với từng giống
@@ -29,14 +22,184 @@ namespace Meow_Meow_Cattery
             MeowMeowCatteryRepo.addCat(154, eBreeds.Toyger, "White");
 
 
-            //Test Search
-            MeowMeowCatteryRepo.Search(eBreeds.Sphynx);
-            MeowMeowCatteryRepo.Search(eBreeds.Somali);
-            MeowMeowCatteryRepo.Search(eBreeds.Burmese);
+            Console.WriteLine("        Welcome to MeowMeow Cattery!"); //InputYourChoose          
+            Console.WriteLine("=============================================");
+            Console.WriteLine("1. Search Cat"); //InputSearch
+            Console.WriteLine("2. Add Cat");
+            Console.WriteLine("3. Create Order");
 
-            //Test Order
-            MeowMeowCatteryRepo.CreateOrder(123, eBreeds.Sphynx, new DateTime(2020, 12, 1));
-            MeowMeowCatteryRepo.CreateOrder(133, eBreeds.Somali, new DateTime(2020, 12, 1));
+            Console.WriteLine();
+            Console.Write("Input Your Choose: ");
+
+            int InputYourChoose = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            switch (InputYourChoose)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("You can search in keywords: Sphynx, Toyger, Somali, Burmese");
+                        Console.WriteLine("===========================================================");
+                        string InputSearch = Console.ReadLine();
+                        switch (InputSearch)
+                        {
+                            case "Sphynx":
+                                {
+                                    MeowMeowCatteryRepo.Search(eBreeds.Sphynx);
+                                    break;
+                                }
+                            case "Toyger":
+                                {
+                                    MeowMeowCatteryRepo.Search(eBreeds.Toyger);
+                                    break;
+                                }
+                            case "Somali ":
+                                {
+                                    MeowMeowCatteryRepo.Search(eBreeds.Somali);
+                                    break;
+                                }
+                            case "Burmese":
+                                {
+                                    MeowMeowCatteryRepo.Search(eBreeds.Burmese);
+                                    break;
+                                }
+                            default:
+                                Console.WriteLine("No Result!");
+                                break;
+                        }
+                        Console.ReadKey();
+                    }
+                    break;
+                case 2:
+                    {
+                        Console.WriteLine("1. Sphynx");
+                        Console.WriteLine("2. Toyger");
+                        Console.WriteLine("3. Somali");
+                        Console.WriteLine("4. Burmese");
+                        Console.WriteLine("==========");
+                        Console.Write("Input Breed: ");
+                        int InputBreed = int.Parse(Console.ReadLine());
+                        {
+                            switch (InputBreed)
+                            {
+                                case 1:
+                                    {
+                                        Console.Write("Input ID: ");
+                                        int InputId = int.Parse(Console.ReadLine());
+                                        Console.Write("Input Color: ");
+                                        string InputColor = Console.ReadLine();
+                                        MeowMeowCatteryRepo.addCat(InputId, eBreeds.Sphynx, InputColor);
+                                        Console.WriteLine("Add successfully: {0} | {1} | {2}", InputId, eBreeds.Sphynx, InputColor);
+                                        Console.WriteLine();
+                                    }
+                                    break;
+                                case 2:
+                                    {
+                                        Console.Write("Input ID: ");
+                                        int InputId = int.Parse(Console.ReadLine());
+                                        Console.Write("Input Color: ");
+                                        string InputColor = Console.ReadLine();
+                                        MeowMeowCatteryRepo.addCat(InputId, eBreeds.Burmese, InputColor);
+                                        Console.WriteLine("Add successfully: {0} | {1} | {2}", InputId, eBreeds.Burmese, InputColor);
+                                        Console.WriteLine();
+                                    }
+                                    break;
+                                case 3:
+                                    {
+                                        Console.Write("Input ID: ");
+                                        int InputId = int.Parse(Console.ReadLine());
+                                        Console.Write("Input Color: ");
+                                        string InputColor = Console.ReadLine();
+                                        MeowMeowCatteryRepo.addCat(InputId, eBreeds.Toyger, InputColor);
+                                        Console.WriteLine("Add successfully: {0} | {1} | {2}", InputId, eBreeds.Toyger, InputColor);
+                                        Console.WriteLine();
+                                    }
+                                    break;
+                                case 4:
+                                    {
+                                        Console.Write("Input ID: ");
+                                        int InputId = int.Parse(Console.ReadLine());
+                                        Console.Write("Input Color: ");
+                                        string InputColor = Console.ReadLine();
+                                        MeowMeowCatteryRepo.addCat(InputId, eBreeds.Somali, InputColor);
+                                        Console.WriteLine("Add successfully: {0} | {1} | {2}", InputId, eBreeds.Somali, InputColor);
+                                        Console.WriteLine();
+                                    }
+                                    break;
+                            }
+                            Console.ReadKey();
+                        }
+                    }
+                    break;
+                    
+
+                case 3:
+                    {
+                        Console.WriteLine("Create Order");
+                        Console.WriteLine("============");
+                        Console.WriteLine("1. Sphynx");
+                        Console.WriteLine("2. Toyger");
+                        Console.WriteLine("3. Somali");
+                        Console.WriteLine("4. Burmese");
+                        Console.WriteLine();
+                        Console.Write("Input Breed you want to buy: ");
+                        int InputOrderBreed = int.Parse(Console.ReadLine());
+                        switch (InputOrderBreed)
+                        {
+                            case 1:
+                                {
+                                    MeowMeowCatteryRepo.Search(eBreeds.Sphynx);
+                                    Console.Write("Input ID: ");
+                                    int InputOrderID = int.Parse(Console.ReadLine());
+                                    Console.Write("Input DateTime To Take Meow: ");
+                                    DateTime InputOrderTime = DateTime.Parse(Console.ReadLine());
+                                    MeowMeowCatteryRepo.CreateOrder(InputOrderID, eBreeds.Sphynx, InputOrderTime);
+                                    Console.WriteLine("Create Order Successfully: {0} | {1} | {2}", InputOrderID, eBreeds.Sphynx, InputOrderTime);
+                                    Console.WriteLine();
+                                }
+                                break;
+                            case 2:
+                                {
+                                    MeowMeowCatteryRepo.Search(eBreeds.Toyger);
+                                    Console.Write("Input Meow ID you want to buy: ");
+                                    int InputOrderID = int.Parse(Console.ReadLine());
+                                    Console.Write("Input Color: ");
+                                    DateTime InputOrderTime = DateTime.Parse(Console.ReadLine());
+                                    MeowMeowCatteryRepo.CreateOrder(InputOrderID, eBreeds.Toyger, InputOrderTime);
+                                    Console.WriteLine("Create Order Successfully: {0} | {1} | {2}", InputOrderID, eBreeds.Toyger, InputOrderTime);
+                                    Console.WriteLine();
+                                }
+                                break;
+                            case 3:
+                                {
+                                    MeowMeowCatteryRepo.Search(eBreeds.Somali);
+                                    Console.Write("Input ID: ");
+                                    int InputOrderID = int.Parse(Console.ReadLine());
+                                    Console.Write("Input DateTime To Take Meow you want to buy : ");
+                                    DateTime InputOrderTime = DateTime.Parse(Console.ReadLine());
+                                    MeowMeowCatteryRepo.CreateOrder(InputOrderID, eBreeds.Somali, InputOrderTime);
+                                    Console.WriteLine("Create Order Successfully: {0} | {1} | {2}", InputOrderID, eBreeds.Somali, InputOrderTime);
+                                    Console.WriteLine();
+                                }
+                                break;
+                            case 4:
+                                {
+                                    MeowMeowCatteryRepo.Search(eBreeds.Burmese);
+                                    Console.Write("Input ID: ");
+                                    int InputOrderID = int.Parse(Console.ReadLine());
+                                    Console.Write("Input DateTime To Take Meow you want to buy: ");
+                                    DateTime InputOrderTime = DateTime.Parse(Console.ReadLine());
+                                    MeowMeowCatteryRepo.CreateOrder(InputOrderID, eBreeds.Burmese, InputOrderTime);
+                                    Console.WriteLine("Create Order Successfully: {0} | {1} | {2}", InputOrderID, eBreeds.Burmese, InputOrderTime);
+                                    Console.WriteLine();
+                                }
+                                break;
+                        }
+
+                    }
+                    break;
+            }
         }
     }
 }
